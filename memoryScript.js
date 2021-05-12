fb6b286a744d189a2041196e3e7e967e5046dca6
Daniel Pericich <danielpericich@Daniels-MBP.attlocal.net>
2021-05-11 19:18:55 -0500

    refactored and organized functions

--- memoryScript.js
+++ memoryScript.js
@@ -21,6 +21,12 @@ let cardsArray = [];
 
 
 
+///////////////////////////////////////////////////////
+//////////////////   FUNCTIONS   //////////////////////
+///////////////////////////////////////////////////////
+
+
+// Game Setup
 const displayWinBanner = () => {
   displayBannerWin.classList.remove('hidden');
 }
@@ -37,48 +43,7 @@ const hideLoseBanner = () => {
   displayBannerLose.classList.add("hidden")
 }
 
-const addCardToSelected = (event) => {
-  // Add logic to check if card is already in
-  if (selectedCards.length < 1) {
-    const selectedCard = event.target
-    selectedCards.push({name: selectedCard.dataset.name, id: selectedCard.dataset.id})
-    console.log(selectedCards)
-  } else {
-    selectedCards.push()
-
-  }
-}
-
-// Need to refactor flipCard to flipCard(id) and flipCardAction(event)
-
-const compareCards = (selectedCard1, selectedCard2) => {
-  const card1 = selectedCard1.dataset;
-  const card2 = selectedCard2.dataset;
-  if (card1.name === card2.name) {
-    removeCardFromPlay(card1.id);
-    removeCardFromPlay(card2.id);
-    updateScore();
-  } else {
-    flipCard(card1.id);
-    flipCard(card2.id);
-  }
-}
-
-// Target card object by id or data-name or data-id
-
-const removeCardFromPlay = (id) => {
-  const playingCard = document.querySelector(`[data-id="${id}"]`)
-  playingCard.removeEventListener('click', flipCard);
-  playingCard.classList.add("playing-card-disabled")
-}
-
-// Card, Deck and Grid Setup
-const incrementCount = () => {
-  let previousCount = parseInt(cardCount.textContent);
-  let newCount = previousCount + 1;
-  cardCount.innerHTML = newCount;
-}
-
+// Card Setup
 const flipCard = (id) => {
   let selectedCard = document.querySelector(`[data-id="${id}"]`)
   let imgPathValue = selectedCard.getAttribute("src")
@@ -96,8 +61,12 @@ const eventFlipCard = (event) => {
   flipCard(id)
 }
 
-const getCardName = (imgSrc) => {
-  return imgSrc.split("/")[1]
+const removeCardFromPlay = (id) => {
+  console.log("I was called")
+  const playingCard = document.querySelector(`[data-id="${id}"]`);
+  playingCard.removeEventListener('click', flipCard, true);
+  playingCard.removeEventListener('click', addCardToSelected, true);
+  playingCard.classList.add("playing-card-disabled")
 }
 
 const createCard = (i) => {
@@ -108,20 +77,21 @@ const createCard = (i) => {
   card.setAttribute('data-name', getCardName(cardsLinks[i]));
   card.setAttribute('data-id', i)
   card.addEventListener('click', eventFlipCard);
-  card.addEventListener('click', addCardToSelected)
+  card.addEventListener('click', addCardToSelected);
   return card;
 }
+
+const getCardName = (imgSrc) => {
+  return imgSrc.split("/")[1]
+}
+
+// Grid Setup
 
 const populateDeck = () => {
   for (let i = 0; i < 10; i++) {
     let card = createCard(i);
     cardsArray.push(card)
   }
-
-  // for (let i = 0; i < cardsArray.length; i++) {
-  //   grid.appendChild(cardsArray[i])
-  //   grid.appendChild(cardsArray[i])
-  // }
 }
 
 const shuffleDeck = (deck) => {
@@ -134,7 +104,6 @@ const shuffleDeck = (deck) => {
     deck.splice(randomIndex, 1)
     deckLength -= 1;
   }
-
   return shuffledDeck;
 }
 
@@ -151,6 +120,66 @@ const resetGrid = () => {
   }
 }
 
+// Game Rules
+
+const updateScore = () => {
+  cardCount.innerHTML = parseInt(cardCount.textContent) - 2
+}
+
+const updateGuesses = () => {
+  let guesses = parseInt(guessCount.innerHTML)
+  guesses += 1;
+  guessCount.innerHTML = guesses;
+}
+
+const incrementCount = () => {
+  let previousCount = parseInt(cardCount.textContent);
+  let newCount = previousCount + 1;
+  cardCount.innerHTML = newCount;
+}
+
+
+const addCardToSelected = (event) => {
+  // Add logic to check if card is already in
+  const selectedCard = event.target
+  if (selectedCards.length < 1) {
+    selectedCards.push(selectedCard)
+    console.log(selectedCards)
+  } else {
+    compareCards(selectedCards[0], selectedCard)
+  }
+}
+
+const emptySelectedCards =  () => {
+  selectedCards = []
+}
+
+const compareCards = (selectedCard1, selectedCard2) => {
+  const card1 = selectedCard1.dataset;
+  const card2 = selectedCard2.dataset;
+  if (card1.name === card2.name && card1.id !== card2.id) {
+    removeCardFromPlay(card1.id);
+    removeCardFromPlay(card2.id);
+    updateScore();
+    updateGuesses();
+    emptySelectedCards();
+    console.log(cardCount)
+    if(cardCount.innerHTML === "0") {
+      displayWinBanner();
+    }
+  } else if (card1.name !== card2.name && parseInt(guessCount) >= 8) {
+    displayLoseBanner();
+  } else {
+    flipCard(card1.id);
+    flipCard(card2.id);
+    emptySelectedCards();
+  }
+}
+
+
+
+
+
 const resetGame = () => {
   resetGrid();
   populateDeck();
@@ -160,49 +189,8 @@ const resetGame = () => {
   // Reset Guesses Counter to 0
   hideWinBanner();
   hideLoseBanner();
+  selectedCards = [];
 }
-
-// Game Logic
-/*
-1) When card is selected check if array is empty
-2) If array is empty then push node in
-3) If array is not empty, check if the data-names are the same
-4) If so change the border style on both cards to orange-red, move the data-ids to the found cards array and empty out check queue
-5) If not flip both cards back over and empty out the queue
-6) The game is over when the guessed card length is equal to 10
-*/
-
-/* Game Logic
-1) New fresh game and board is displayed
-2) Player clicks on a single card
-3) selectedCard Array checks it's length to determine what to do
-  3A) If array is empty we push this card to the array
-  3B) If array has a lenght of one then we need to compare the cards
-    3a) If cards have the same data-name:
-      1) change the style of both cards to have a red border
-      2) remove the hover effect and increase size of card to 1.07
-      3) remove eventListener
-      4) empty out array of selected cards
-      5) increment guesses made by 1
-      6) subtract 2 from the cards remaining count
-        1) If cards remaining count is 0, set alert for game won
-    3b) If cards do not have teh same data-name:
-      1) increment guesses made by 1
-      2) flip over both cards
-      3) empty out the array of selected cards
-
-*/
-
-
-const selectCard = () => {
-
-}
-
-const compareSelectedCards= () => {
-
-}
-
-
 
 // Setup Events
 resetButton.addEventListener("click", resetGame)
